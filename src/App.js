5f253eae297e30323816648e4e2e24086477cea0
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2023-02-09 03:13:56 +0530

    line and pie chart completed

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Invoices from "./scenes/invoices"
 import Form from "./scenes/form"
 import FAQ from "./scenes/faq"
 import Bar from "./scenes/bar"
-// import Line from "./scenes/line"
-// import Pie from "./scenes/pie"
+import Pie from "./scenes/pie"
+import Line from "./scenes/line"
 // import Geography from "./scenes/geography"
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
@@ -37,8 +37,8 @@ function App() {
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/bar" element={<Bar />} />
-              {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/line" element={<Line />} /> */}
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
